f94fef88fbc8cd29d45c054f929c6c48ad294570
shutingting <shutingting@100tal.com>
2019-12-09 17:48:20 +0800

    init project

---
+++ src/proto/reader.go
@@ -0,0 +1,11 @@
+package proto
+
+import "bufio"
+
+type Reader struct {
+	re *bufio.Reader
+}
+
+func (r *Reader) readInt8()  {
+
+}
---
+++ src/proto/writer.go
@@ -0,0 +1,12 @@
+package proto
+
+import "bufio"
+
+type writer struct {
+	w *bufio.Writer
+	
+}
+
+func (w *writer) writeArgs()  {
+	
+}
